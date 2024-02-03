1016ae85c2dbdd074689db02f425fe323c505415
Gitsanj66 <sanjanamsr.23@gmail.com>
2024-02-03 17:44:24 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(cors());
+app.use("/", express.static("public"))
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+//app.use("/",express.static("public"));
 
+const budget = require("./data.json");
+console.log(budget)
+
+app.get("/hello",(req,res) => {
+    res.send("Hello Sanj!!");
+})
 
 app.get('/budget', (req, res) => {
     res.json(budget);
